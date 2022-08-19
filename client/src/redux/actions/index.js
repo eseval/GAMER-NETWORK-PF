62c2debb5c7bcede16f189fd0c15e6f25ebca832
Facundo <facufaccioli18@gmail.com>
2022-08-19 17:59:01 -0300

    modificado redux con las rutas get de users y news

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,14 +1,60 @@
 import axios from "axios";
-import { GET_USERS } from "./types";
+import { GET_USERS, GET_USERS_BY_ID, USERS_LOADING, USER_LOADING, GET_ALL_NEWS, ALL_NEWS_LOADING, GET_NEWS_BY_ID, NEWS_LOADING } from "./types";
+
+const USERS_URL = 'https://pf-henry-gamesportal.herokuapp.com/users';
+const NEWS_URL = 'https://pf-henry-gamesportal.herokuapp.com/news';
+
 
 export function getUsers() {
   return async function (dispatch) {
     try {
-      let json = await axios.get("http://localhost:3001/users");
-      return dispatch({
-        type: GET_USERS,
-        payload: json.data,
-      });
+      let json = await axios.get(USERS_URL);
+      return (
+        dispatch({type: USERS_LOADING}),
+        dispatch({type: GET_USERS, payload: json.data})
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+export function getUserById(id) {
+  return async function (dispatch) {
+    try {
+      let json = await axios.get(`${USERS_URL}/${id}`);
+      return (
+        dispatch({type: USER_LOADING}),
+        dispatch({type: GET_USERS_BY_ID, payload: json.data})
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+export function getAllNews() {
+  return async function (dispatch) {
+    try {
+      let json = await axios.get(NEWS_URL);
+      return (
+        dispatch({type: ALL_NEWS_LOADING}),
+        dispatch({type: GET_ALL_NEWS, payload: json.data})
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+export function getNewsById(id) {
+  return async function (dispatch) {
+    try {
+      let json = await axios.get(`${NEWS_URL}/${id}`);
+      return (
+        dispatch({type: NEWS_LOADING}),
+        dispatch({type: GET_NEWS_BY_ID, payload: json.data})
+      );
     } catch (error) {
       console.log(error);
     }
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,15 +1,65 @@
-import { GET_USERS } from "../actions/types"
+import { GET_USERS, GET_USERS_BY_ID, USERS_LOADING, USER_LOADING, GET_ALL_NEWS, ALL_NEWS_LOADING, GET_NEWS_BY_ID, NEWS_LOADING } from "../actions/types"
 
 const initialState = {
-  users: []
+  users: [],
+  isLoadinsUsers: true,
+  user: {},
+  isLoadingUser: true,
+  allNews: [],
+  isLoadingAllNews: true,
+  news: {},
+  isLoadingNews: true
 }
 
 export default function rootReducer(state= initialState, action) {
   switch (action.type) {
+    case USERS_LOADING: {
+      return {
+        ...state,
+        isLoadingUsers: true
+      }
+    }
     case GET_USERS:
       return {
         ...state,
-        users: action.payload
+        users: action.payload,
+        isLoadingUsers: false
+      }
+    case USER_LOADING: {
+      return {
+        ...state,
+        isLoadingUser: true
+      }
+    }
+    case GET_USERS_BY_ID:
+      return {
+        ...state,
+        user: action.payload,
+        isLoadingUser: false
+      }
+    case ALL_NEWS_LOADING: {
+      return {
+        ...state,
+        isLoadingAllNews: true
+      }
+    }
+    case GET_ALL_NEWS:
+      return {
+        ...state,
+        allNews: action.payload,
+        isLoadingAllNews: false
+      }
+    case NEWS_LOADING: {
+      return {
+        ...state,
+        isLoadingNews: true
+      }
+    }
+    case GET_NEWS_BY_ID:
+      return {
+        ...state,
+        news: action.payload,
+        isLoadingNews: false
       }
     default:
       return {...state}
