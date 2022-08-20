dbfc667f6f71389a9e48f31c1badc237604d0499
eseval <90922661+eseval@users.noreply.github.com>
2022-08-20 08:20:24 -0500

    Update index.js
    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
