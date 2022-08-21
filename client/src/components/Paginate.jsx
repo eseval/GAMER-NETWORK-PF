fe2082afc254b85ef98bcba25b7a6a5607d51fcc
Patricio Alassia Demo <patoad13@gmail.com>
2022-08-20 21:11:47 -0300

    Paginado con el comentario para home

--- client/src/components/Paginate.jsx
+++ client/src/components/Paginate.jsx
@@ -17,3 +17,28 @@ export default function Paginate ({ newsPerPage, allNews, paginate }) {
         </nav>
     );
 }
+
+
+
+//ESTO VA EN EL COMPONENTE HOME:
+
+// const [currentPage, setCurrentPage] = useState(1);
+// const [newsPerPage] = useState(12);
+// const indexOfLastNew = currentPage * newsPerPage;
+// const indexOfFirstNew = indexOfLastNew - newsPerPage;
+// const currentNews = allNews.slice(indexOfFirstNew, indexOfLastNew);
+
+// if (currentPage > Math.ceil(allNews.length / newsPerPage) && currentPage !== 1) {
+//     setCurrentPage(1);
+// }
+
+// const paginate = (pageNumber) => {
+//     setCurrentPage(pageNumber);
+// };
+
+// function handleClick(e) {
+//     e.preventDefault()
+//     dispatch(getNews()) //getNews es un nombre ficticio, poner el nombre de la variable que 
+                          //traiga todas las noticias, sacado del index de Actions
+// };
+// 
