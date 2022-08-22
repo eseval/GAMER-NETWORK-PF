27594bc339a70d96bd7a78ea99b78d29f02efa84
Facundo <facufaccioli18@gmail.com>
2022-08-22 14:37:54 -0300

    listo

--- client/src/components/NewsContainer.jsx
+++ client/src/components/NewsContainer.jsx
@@ -1,35 +1,9 @@
-/* import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
-import NewsCard from "./NewsCard"
-import { getAllNews } from "../redux/actions";
-
-export default function NewsContainer() {
-  // ESTA RUTA VA A CONTENER TODAS LAS NOTICIAS CON FILTRADO Y LAS VA A PASAR A NEWS CARD
-  // NO TE OLVIDES DEL FILTRADO!!
-  const dispatch = useDispatch();
-  const allNews = useSelector(state => state.allNews);
-  
-  useEffect(() => {
-    dispatch(getAllNews())
-  }, [dispatch]);
-  
-  return (
-    <div className="container">
-      <h1 className="m-5 text-5xl font-semibold text-center text-white">News</h1>
-      <div className="container flex flex-wrap justify-center">
-        {allNews && allNews.map(news => {
-          return <NewsCard news={news} />
-        })}
-      </div>
-    </div>
-  )
-} */
-
 import { useEffect , useState} from "react";
 import { useDispatch, useSelector } from "react-redux"
 import NewsCard from "./NewsCard"
 import { getAllNews } from "../redux/actions";
 import Paginate from "./Paginate.jsx";
+import OrderNewsByTitle from "./OrderNewsByTitle";
 
 export default function NewsContainer() {
   // ESTA RUTA VA A CONTENER TODAS LAS NOTICIAS CON FILTRADO Y LAS VA A PASAR A NEWS CARD
@@ -58,6 +32,7 @@ export default function NewsContainer() {
   return (
     <div className="container">
       <h1 className="m-5 text-5xl font-semibold text-center text-white">News</h1>
+      <OrderNewsByTitle/>
       <div className="container flex flex-wrap justify-center">
         {typeof allNews === "object"  ? currentNews.map(news => {
           return <NewsCard news={news} key={news.id}/>
