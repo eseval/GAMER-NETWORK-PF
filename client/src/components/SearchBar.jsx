ad0c8502ed9fa81c160ebcded8ace6e840076ae4
sdin2 <77481421+sdin2@users.noreply.github.com>
2022-08-22 12:51:53 -0300

    Update SearchBar.jsx

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,77 +1,46 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { getNewsByTitle } from "../redux/actions";
-import styled from "styled-components";
 import { BsSearch } from "react-icons/bs";
+import styledSearchBar from "../styles/SearchBar";
+import {searchBarsearch , getAllNews} from "../redux/actions/index"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+const { Container, Wrapper, Input, Button } = styledSearchBar();
 
-const Container = styled.div`
-  height: 30px;
-  background-color: whitesmoke;
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 14px;
-  font-weight: 500;
-`
-
-const Wrapper = styled.div`
-  padding: 10px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`
-
-const Input = styled.input`
-  border: none;
-`
-
-const Button = styled.button`
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-  color: black;
-  size: 20px;
-`
-
-
-export default function SearchBar() {
-  const [title, setTitle] = useState('');
+export default function Search(){
+  
   const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    dispatch(getAllNews())
+  }, [dispatch]);
+
+  const allNews = useSelector(state => state.allNews);
 
   function handleOnChange(e) {
     e.preventDefault();
     setTitle(e.target.value);
+    dispatch(searchBarsearch(e.target.value));
+
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    dispatch(getNewsByTitle(title));
-    setTitle('');
-  }
-
-  function handleKeyPress(e) {
-    if (e.key === "Enter") {
-      handleSubmit(e);
-    } else if (title[0] === " ") {
-      setTitle(title.substring(1));
-    }
-  }
 
   return (
-      <Container>
-        <Wrapper>
-          <Input
-              type="text"
-              placeholder="Search for a news"
-              value={ title }
-              onChange={ (e) => handleOnChange(e) }
-              onKeyDown={ (e) => handleKeyPress(e) }
-          />
-          <Button
-              type="submit"
-              onClick={ (e) => handleSubmit(e) }><BsSearch/></Button>
+    <Container>
+      <Wrapper>
+        <Input
+          type="text"
+          placeholder="Search for a news"
+          onChange={handleOnChange}
+          list="data"
+          id="inputSearch"
+        />
+        <datalist id="data">
+                {typeof allNews === "object"  ? allNews.map(e =>
+                    <option key={e.id} value={e.title}></option>
+                ): ""}
+        </datalist>
         </Wrapper>
-      </Container>
-  )
+    </Container>
+  );
 }
