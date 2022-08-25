2ab43947b451924a590bb9d4d96edde286ee0450
eseval <90922661+eseval@users.noreply.github.com>
2022-08-25 08:53:42 -0500

    feat/forumAnswerCont

--- src/components/ForumAnswers.jsx
+++ src/components/ForumAnswers.jsx
@@ -14,34 +14,36 @@ export default function ForumAnswers() {
   }
 
   return (
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 mx-5 mb-10 max-w-screen-md ">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 mx-5 mb-10 max-w-screen-md my-3">
         { forumAnswers.map((answer) => (
-            <div className="mx-5 my-6 px-4">
-              <div className="flex my-2">
-                <img
-                    className="w-12 h-12 rounded-full mr-2"
-                    src={ answer.img }
-                    alt=""
-                />
-                <span className="text-base font-bold">{ answer.username }</span>
-              </div>
-              <div className="flex-1 ">
-                <p className="text-gray-700 text-base">{ answer.answer }</p>
-              </div>
-              <div className="flex">
-                <button
-                    className="my-2 cursor-pointer mx-1"
-                    onClick={ handleOnClick1 }
-                >
-                  <AiFillLike/>
-                </button>
-                <button
-                    className="my-2 cursor-pointer mx-1"
-                    onClick={ handleOnClick2 }
-                >
-                  <AiFillDislike/>
-                </button>
-                <p className="px-2">{ likes }</p>
+            <div className="border">
+              <div className="mx-5 my-6 px-4">
+                <div className="flex my-2">
+                  <img
+                      className="w-12 h-12 rounded-full mr-2"
+                      src={ answer.img }
+                      alt=""
+                  />
+                  <span className="text-base font-bold">{ answer.username }</span>
+                </div>
+                <div className="flex-1 ">
+                  <p className="text-gray-700 text-base">{ answer.answer }</p>
+                </div>
+                <div className="flex">
+                  <button
+                      className="my-2 cursor-pointer mx-1"
+                      onClick={ handleOnClick1 }
+                  >
+                    <AiFillLike/>
+                  </button>
+                  <button
+                      className="my-2 cursor-pointer mx-1"
+                      onClick={ handleOnClick2 }
+                  >
+                    <AiFillDislike/>
+                  </button>
+                  <p className="px-2">{ likes }</p>
+                </div>
               </div>
             </div>
         )) }
