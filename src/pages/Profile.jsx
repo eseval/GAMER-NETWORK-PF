721b8cc0a7f4764104b7c580590e4ca76c59ebff
Facundo <facufaccioli18@gmail.com>
2022-08-31 15:32:04 -0300

    cambios en perfil y en formulario de usuario

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -57,6 +57,10 @@ export default function Profile() {
           <div className="border-t border-gray-200">
             <dl>
               <div className="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Description</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ user.description }</dd>
+              </div>
+              <div className="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">Email</dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ user.email }</dd>
               </div>
