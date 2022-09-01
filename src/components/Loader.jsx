2a8b58f88ee582ae985502b32ea17303f6db0541
TotiFz <totifernandez10@gmail.com>
2022-09-01 11:41:32 -0300

    css home y news

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import loaderGif from '../assets/loader/loader.gif';
 
 export default function Loader() {
-  return (
-    <div>
-      <img src={loaderGif} alt='loader' className={'w-8 mx-auto'} />
-    </div>
-  );
+	return (
+		<div>
+			<img
+				src="https://c.tenor.com/pw9ZsUdsEYgAAAAj/capoo-blue-cat.gif"
+				alt="loader"
+				className={'w-50 mx-auto'}
+			/>
+		</div>
+	);
 }
