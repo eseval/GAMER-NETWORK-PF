36197bd77ba54bd157070cca0c0d2971ddb53898
Facundo <facufaccioli18@gmail.com>
2022-08-31 21:31:05 -0300

    agregado forum dashboard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import NewsDashboard from "../components/AdminDashboard/NewsDashboard";
 import NavBar from "../components/NavBar";
 import UsersDashboard from "../components/AdminDashboard/UsersDashboard";
+import ForumDashboard from "../components/AdminDashboard/ForumDashboard";
 
 export default function AdminDashboard() {
   const navigate = useNavigate();
@@ -20,6 +21,8 @@ export default function AdminDashboard() {
     component = <UsersDashboard />
   } else if (dash === 'NewsDashboard') {
     component = <NewsDashboard />
+  } else if (dash === 'ForumDashboard') {
+    component = <ForumDashboard />
   }
 
 
@@ -27,9 +30,31 @@ export default function AdminDashboard() {
   return(
     <div>
       <NavBar />
-      <div>
-        <button onClick={() => setDash('UsersDashboard')}>Users</button>
-        <button onClick={() => setDash('NewsDashboard')}>News</button>
+      <div className="container flex flex-row my-5">
+        <button 
+          className="relative inline-flex items-center justify-center p-0.5 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+          onClick={() => setDash('UsersDashboard')}
+        >
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Users
+          </span>
+        </button>
+        <button 
+          className="relative inline-flex items-center justify-center p-0.5 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+          onClick={() => setDash('NewsDashboard')}
+        >
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            News
+          </span>
+        </button>
+        <button 
+          className="relative inline-flex items-center justify-center p-0.5 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+          onClick={() => setDash('ForumDashboard')}
+        >
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Forum
+          </span>
+        </button>
       </div>
       {component}
     </div>
